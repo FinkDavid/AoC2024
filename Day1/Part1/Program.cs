ed238464ba10894e29d3362e65651bce735483ec
David Fink <itzcrazyzockt@gmail.com>
2024-12-01 14:12:46 +0100

    Day1: Part1/Part2 completed

---
+++ Day1/Part1/Program.cs
@@ -0,0 +1,25 @@
+﻿﻿string[] lines = File.ReadAllLines("../input.txt");
+
+List<int> leftNumbers = new List<int>();
+List<int> rightNumbers = new List<int>();
+
+foreach (string line in lines)
+{
+    leftNumbers.Add(int.Parse(line.Split(' ')[0]));
+    rightNumbers.Add(int.Parse(line.Split(' ')[3]));
+}
+
+int totalDistances = 0;
+
+for (int i = 0; i < leftNumbers.Count; i++)
+{
+    int smallestLeftNumber = leftNumbers.Min();
+    int smallestRightNumber = rightNumbers.Min();
+
+    totalDistances += Math.Abs(smallestLeftNumber - smallestRightNumber);
+
+    leftNumbers.Remove(smallestLeftNumber);
+    rightNumbers.Remove(smallestRightNumber);
+}
+
+Console.WriteLine("Result: " + totalDistances);
---
+++ Day1/Part2/Program.cs
@@ -0,0 +1,20 @@
+﻿﻿string[] lines = File.ReadAllLines("../input.txt");
+
+List<int> leftNumbers = new List<int>();
+List<int> rightNumbers = new List<int>();
+
+foreach (string line in lines)
+{
+    leftNumbers.Add(int.Parse(line.Split(' ')[0]));
+    rightNumbers.Add(int.Parse(line.Split(' ')[3]));
+}
+
+int similarityScore = 0;
+
+for (int i = 0; i < leftNumbers.Count; i++)
+{
+    int sameRightNumbers = rightNumbers.FindAll(x => x == leftNumbers[i]).Count;
+    similarityScore += leftNumbers[i] * sameRightNumbers;
+}
+
+Console.WriteLine("Result: " + similarityScore);
