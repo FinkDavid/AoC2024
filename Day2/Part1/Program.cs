273d26e205f6c3f6cef7b9ca17875e07f556e81e
David Fink <itzcrazyzockt@gmail.com>
2024-12-02 14:31:41 +0100

    Day2: Part1/Part2 completed

---
+++ Day2/Part1/Program.cs
@@ -0,0 +1,52 @@
+﻿string[] lines = File.ReadAllLines("../input.txt");
+
+int safeReports = 0;
+int maxDistance = 3;
+
+foreach (string line in lines)
+{
+    List<int> numbers = line.Split(' ').Select(int.Parse).ToList();
+    
+    if (numbers[1] > numbers[0])
+    {
+        if (CheckIfSafeReport(numbers, true))
+        {
+            safeReports++;
+        }
+    }
+    else if (numbers[1] < numbers[0])
+    {
+        if (CheckIfSafeReport(numbers, false))
+        {
+            safeReports++;
+        }
+    }
+}
+
+Console.WriteLine("Result: " + safeReports);
+
+bool CheckIfSafeReport(List<int> numbers, bool isIncreasing)
+{
+    bool isSafeReport = true;
+    for (int i = 0; i < numbers.Count - 1; i++)
+    {
+        if (isIncreasing)
+        {
+            if (numbers[i + 1] <= numbers[i] || numbers[i + 1] - numbers[i] > maxDistance)
+            {
+                isSafeReport = false;
+                break;
+            }
+        }
+        else
+        {
+            if (numbers[i + 1] >= numbers[i] || numbers[i] - numbers[i + 1] > maxDistance)
+            {
+                isSafeReport = false;
+                break;
+            }
+        }
+    }
+
+    return isSafeReport;
+}
---
+++ Day2/Part2/Program.cs
@@ -0,0 +1,72 @@
+﻿string[] lines = File.ReadAllLines("../input.txt");
+
+int safeReports = 0;
+int maxDistance = 3;
+
+foreach (string line in lines)
+{
+    List<int> numbers = line.Split(' ').Select(int.Parse).ToList();
+
+    if (CheckIfSafeReport(numbers, true))
+    {
+        safeReports++;
+    }
+    else
+    {
+        if (CheckIfSafeReport(numbers, false))
+        {
+            safeReports++;
+        }
+        else
+        {
+            // Just bruteforcing my way through it and trying to remove every number to see if that makes it work
+            for (int i = 0; i < numbers.Count; i++)
+            {
+                List<int> tempNumbers = new List<int>(numbers);
+                tempNumbers.RemoveAt(i);
+
+                if (CheckIfSafeReport(tempNumbers, true))
+                {
+                    safeReports++;
+                    break;
+                }
+                else
+                {
+                    if (CheckIfSafeReport(tempNumbers, false))
+                    {
+                        safeReports++;
+                        break;
+                    }
+                }
+            }
+        }
+    }
+}
+
+Console.WriteLine("Result: " + safeReports);
+
+bool CheckIfSafeReport(List<int> numbers, bool isIncreasing)
+{
+    bool isSafeReport = true;
+    for (int i = 0; i < numbers.Count - 1; i++)
+    {
+        if (isIncreasing)
+        {
+            if (numbers[i + 1] <= numbers[i] || numbers[i + 1] - numbers[i] > maxDistance)
+            {
+                isSafeReport = false;
+                break;
+            }
+        }
+        else
+        {
+            if (numbers[i + 1] >= numbers[i] || numbers[i] - numbers[i + 1] > maxDistance)
+            {
+                isSafeReport = false;
+                break;
+            }
+        }
+    }
+
+    return isSafeReport;
+}
